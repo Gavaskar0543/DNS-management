b4042fffb32811a31b19d39aa830496d2b95af37
Gavaskar <gavaskaraeg@gmail.com>
2024-04-03 14:06:20 +0000

    npx create-react-app

--- Backend/index.js
+++ Backend/index.js
@@ -1,1 +1,13 @@
-console.log("om namo narayana");
+const express = require('express');
+const port = 8000;
+const app = express();
+
+
+
+app.listen(port,(err)=>{
+    if(err){
+        console.log('Error in listening server',err.message);
+        return;
+    }
+    console.log(`Server up and running in port:${port}`);
+})
